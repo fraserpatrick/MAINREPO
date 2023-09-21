29553e53360d988c8a074bee64b0d6711cbd7ef1
Fraser Patrick <fraserp2004@gmail.com>
2023-09-21 16:13:34 +0100

    finished exercise

--- C/exercise02.c
+++ C/exercise02.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 int main(void){
+  int age = 0;
+  char name[20] = "";
+  char color[20] = "";
 
   printf("What is the name? ");
-  scanf("%s", name);
-  printf("What is the age? ");
-  scanf("%d", age);
+  scanf("%19s", name);
   printf("What is the favourite colour? ");
-  scanf("%s", color);
+  scanf("%19s", color);
+  printf("What is the age? ");
+  scanf("%d", &age);
 
-  printf("%s is %d and %s's favourite colour is %s \n", name, age, name, color);
+  printf("%s's favourite colour is %s and %s is %d. \n", name, color, name, age);
   return 0;
 }
