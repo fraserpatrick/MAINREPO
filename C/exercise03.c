0a4e345952b6a83ca55190ce32fb7625414fc610
Fraser <fraser.patrick.2022@uni.strath.ac.uk>
2023-09-26 15:21:23 +0100

    finished exercise

--- C/exercise03.c
+++ C/exercise03.c
@@ -2,6 +2,16 @@
 
 int main(void){
 
-  
+  char c = 'a';
+  short s = 10;
+  int i = 100;
+  long int l = 1000;
+  float f = 1.1;
+  double d = 2.2;
+  int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+  char str[6] = "Hello";
+
+  printf("%c, %d, %d, %ld, %f, %lf, %d, %s\n", c, s, i, l, f, d, arr[4], str);
+
   return 0;
 }
