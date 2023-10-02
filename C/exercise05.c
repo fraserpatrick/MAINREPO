8f503f0cb0df9183c41c41c0b032ac09e74768c9
Fraser Patrick <fraserp2004@gmail.com>
2023-10-02 18:09:06 +0100

    fully finished exercise

--- C/exercise05.c
+++ C/exercise05.c
@@ -7,29 +7,67 @@ int main(void){
     if ((number % 3) == 0){
       if ((number % 5) == 0){
         if ((number % 7) == 0){
-          printf("FizzBuzzBazz\n");
+          if ((number % 9) == 0){
+            printf("FizzBuzzBazzBop\n");
+          }
+          else{
+            printf("FizzBuzzBazz\n");
+          }
+        }
+      else if ((number % 9) == 0){
+        printf("FizzBuzzBop\n");
         }
       else{
         printf("FizzBuzz\n");
-        }
+      }
       }
     else if ((number % 7) == 0){
-      printf("FizzBazz\n");
+      if ((number % 9) == 0){
+        printf("FizzBazzBop\n");
+      }
+      else{
+        printf("FizzBazz\n");
+      }
     }
-    else{
+    else if ((number % 9) == 0){
+      printf("FizzBop\n");
+    }
+    else {
       printf("Fizz\n");
     }
   }
+
+
     else if ((number % 5) == 0){
       if ((number % 7) == 0){
-        printf("BuzzBazz\n");
+        if ((number % 9) == 0){
+          printf("BuzzBazzBop\n");
+        }
+        else{
+          printf("BuzzBazz\n");
+        }
+      }
+      else if ((number % 9) == 0){
+        printf("BuzzBop\n");
       }
       else{
         printf("Buzz\n");
       }
     }
+
+
     else if ((number % 7) == 0){
-      printf("Bazz\n");
+      if ((number % 9) == 0){
+        printf("BazzBop\n");
+      }
+      else {
+        printf("Bazz\n");
+      }
+    }
+
+
+    else if ((number % 9) == 0){
+      printf("Bop\n");
     }
     else{
       printf("%d\n", number);
