365bcb513267e880ac724e072cefa62be095a077
Fraser Patrick <fraserp2004@gmail.com>
2023-10-02 17:07:58 +0100

    actually finished exercise

--- C/exercise04.c
+++ C/exercise04.c
@@ -4,7 +4,7 @@ int main(void){
 
   int input = 0;
   int output = 0;
-  printf("Enter a two digit positive integer: ");
+  printf("Enter a two-digit positive integer: ");
   scanf("%d", &input);
 
   if(input <= 9 || input >= 100){
@@ -16,8 +16,12 @@ int main(void){
       output = output * 10 + digit;
       input /= 10;
     }
-
-    printf("The reversal is: %d \n", output);
+    if (output < 10){
+      printf("The reversal is: 0%d \n", output);  
+    }
+    else{
+      printf("The reversal is: %d \n", output);
+    }
   }
 
   return 0;
