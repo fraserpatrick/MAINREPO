b37dd8156cc4d9bda723df3820ca3c2dae586138
Fraser <fraser.patrick.2022@uni.strath.ac.uk>
2023-10-03 14:49:59 +0100

    started file

--- C/exercise06.c
+++ C/exercise06.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 
+static const int digits[10][7] = {}
+
 int main(void){
+  int input = 0;
+  prinf("Enter a single digit: ")
+  scanf("%d", input)
+
   return 0;
+}
+
+void print_digit(int digit) {
+    printf("\n %c \n",  (digits[digit][0]) ? '-' : ' ');
+    printf("%c %c\n", (digits[digit][1]) ? '|' : ' ', (digits[digit][2]) ? '|' : ' ');
+    printf(" %c \n",  (digits[digit][3]) ? '-' : ' ');
+    printf("%c %c\n", (digits[digit][4]) ? '|' : ' ', (digits[digit][5]) ? '|' : ' ');
+    printf(" %c \n",  (digits[digit][6]) ? '-' : ' ');
 }
