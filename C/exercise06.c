b7129defc731f01615d865278e296ced02f82f7b
Fraser Patrick <fraserp2004@gmail.com>
2023-10-05 22:55:00 +0100

    idk again

--- C/exercise06.c
+++ C/exercise06.c
@@ -1,14 +1,6 @@
 #include<stdio.h>
 
-static const int digits[10][7] = {}
-
-int main(void){
-  int input = 0;
-  prinf("Enter a single digit: ")
-  scanf("%d", input)
-
-  return 0;
-}
+static const int digits[10][7] = {{1,1,1,0,1,1,1},{0,0,1,0,0,1,0},{1,0,1,1,1,0,1},{1,0,1,1,0,1,1},{0,1,1,1,0,1,0},{1,1,0,1,0,1,1},{1,1,0,1,1,1,1},{1,0,1,0,0,1,0},{1,1,1,1,1,1,1},{1,1,1,1,0,1,0}};
 
 void print_digit(int digit) {
     printf("\n %c \n",  (digits[digit][0]) ? '-' : ' ');
@@ -16,4 +8,26 @@ void print_digit(int digit) {
     printf(" %c \n",  (digits[digit][3]) ? '-' : ' ');
     printf("%c %c\n", (digits[digit][4]) ? '|' : ' ', (digits[digit][5]) ? '|' : ' ');
     printf(" %c \n",  (digits[digit][6]) ? '-' : ' ');
+}
+
+int main(void){
+  int input = 'q';
+
+  printf("Enter a single digit: ");
+  scanf("%d", &input);
+
+  while (1){
+    if (input == 'q'){
+      break;
+    }
+    else if (input >= 0 && input <= 9){
+      print_digit(input);
+    }
+    else {
+      printf("Invalid input!\n");
+    }
+    printf("Enter a single digit: ");
+    scanf("%d", &input);
+  }
+  return 0;
 }
