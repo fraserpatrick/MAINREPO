9e82abcbe22b79c88049acab2b82005589ed6442
Fraser Patrick <fraserp2004@gmail.com>
2023-10-13 17:09:00 +0100

    finished exercise

--- C/exercise07.c
+++ C/exercise07.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
 
-void print_array(int* ptr, int len);
+void print_array(int *ptr, int len){
+	int counter;
+	for (counter = 0; counter != len-1; counter++){
+		printf("%d, ", *(ptr + counter));
+	}
+	printf("%d\n", *(ptr + counter));
+}
 
-void print_2d_array(char* ptr, int xlen, int ylen);
+void print_2d_array(char* ptr, int ylen, int xlen){
+	int mainCounter;
+	int lineCounter = 1;
+	for (mainCounter = 0; mainCounter != xlen*ylen; mainCounter++){
+		if (lineCounter == xlen){
+			printf("%c\n", *(ptr + mainCounter));
+			lineCounter = 1;
+		}
+		else {
+			printf("%c, ", *(ptr + mainCounter));
+			lineCounter++;
+		}
+	}
+}
 
 int main(){
-	
+
 	int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	
+
 	print_array(arr, 10);
-	
-	/*This part tests print_2d_array, uncomment it when print_array is working
 
 	char arr2[4][5] = {
 		{'A', 'B', 'C', 'D', 'E'},
@@ -18,10 +35,7 @@ int main(){
 		{'K', 'L', 'M', 'N', 'O'},
 		{'P', 'Q', 'R', 'S', 'T'},
 	};
-	
+
 	print_2d_array(*arr2, 4, 5);
-	
-		*/
 
 }
-
