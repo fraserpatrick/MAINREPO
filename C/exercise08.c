9a02cd7e96e54a37d574d675453c5aedfce93f34
Fraser Patrick <fraserp2004@gmail.com>
2023-10-13 17:40:40 +0100

    finished exercise

--- C/exercise08.c
+++ C/exercise08.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 
-int str_len(char* str);
+int str_len(char* str){
+	int counter = 0;
+	while (*(str+counter) != '\0'){
+		counter++;
+	}
+	return counter;
+}
 
-void str_cpy(char* src, char* dest);
+void str_cpy(char* src, char* dest){
+	int counter = 0;
+	while (counter != str_len(src)){
+		*(dest+counter) = *(src+counter);
+		counter++;
+	}
+	*(dest+counter+1) = '\0';
+}
 
 int main(){
-		
 	char* str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	
+
 	printf("%s is %d characters long!\n", str, str_len(str));
-	
-	/* This part tests only str_cpy, uncomment it when str_len is working
 
 	char str2[100] = "XXXXXXXXXXXXXXXXXXXXXXXXXX   If you're reading this, you probably forgot a '\\0'!";
-	
+
 	str_cpy(str, str2);
-	
+
 	printf("Copied from:\t%s\nCopied to:\t%s", str, str2);
-	
-	*/
 }
