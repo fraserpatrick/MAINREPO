b51deb09bccd5a5838670b43b7b49354d3d2f563
Fraser Patrick <fraserp2004@gmail.com>
2023-10-31 19:54:27 +0000

    finished but coderunner being shite

--- C/CS210/employee.c
+++ C/CS210/employee.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include "employee.h"
-
 
 Employee* new_employee_array(int num){
   Employee* employeeArray = malloc(sizeof(Employee)*num);
@@ -10,15 +11,34 @@ Employee* new_employee_array(int num){
 }
 
 void enter_details(Employee* ptr){
-
+  char fName[20] = "";
+  char lName[20] = "";
+  char job[30] = "";
+  float sal = 0.00;
+  printf("Enter details of employee %d: ", ptr->employee_number);
+  scanf("%s %s %s %f", fName, lName, job, &sal);
+  strcpy(ptr->first_name, fName);
+  strcpy(ptr->last_name, lName);
+  strcpy(ptr->job_title, job);
+  ptr->salary = sal;
 }
 
 Employee* get_highest_paid(Employee* arrayPtr, int num){
-  return arrayPtr;
+  Employee* high = arrayPtr;
+  for (int counter = 0; counter < num-1; counter++){
+    if (arrayPtr->salary < (arrayPtr+1)->salary){
+      high = arrayPtr+1;
+    }
+    arrayPtr++;
+  }
+  return high;
 }
+
 void delete_employee_array(Employee** arrayPtr){
-
+  free(*arrayPtr);
+  *arrayPtr = NULL;
 }
-void print_details(Employee* highestEmp){
 
+void print_details(Employee* highestEmp){
+  printf("----------------------------------\nEmployee Record\n----------------------------------\nEmployee number: %d\nName:\t\t%s, %s\nJob Title:\t%s\nSalary:\t\t%f\n----------------------------------\n",highestEmp->employee_number, highestEmp->last_name, highestEmp->first_name, highestEmp->job_title, highestEmp->salary);
 }
--- C/CS210/employee.h
+++ C/CS210/employee.h
@@ -2,7 +2,7 @@ struct employee{
   int employee_number;
   char first_name[20];
   char last_name[20];
-  char job_title[20];
+  char job_title[30];
   float salary;
 };
 
