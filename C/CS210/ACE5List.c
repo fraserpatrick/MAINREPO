6971b7be91cf21e83b51ff0d45c9a23aa7a63e01
Fraser Patrick <fraserp2004@gmail.com>
2023-11-11 12:49:31 +0000

    added ACE5 files

---
+++ C/CS210/ACE5List.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+Node* new_node(char data[]){
+  Node* nodeloc = malloc(sizeof(Node));
+  nodeloc->value = data;
+  nodeloc->next = NULL;
+  return nodeloc;
+}
+
+void delete_node(Node* nodePtr){
+  free(nodePtr);
+  nodePtr = NULL;
+  }
+
+
+void push(Node* nodePtr, char data[]){
+  while (nodePtr->next != NULL) {
+    nodePtr = nodePtr->next;
+  }
+  nodePtr->next = new_node(data);
+}
