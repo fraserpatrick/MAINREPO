45e370dc63b3504d9af07e50f74594e5a6282edc
Fraser Patrick <fraserp2004@gmail.com>
2023-11-21 15:48:18 +0000

    added ACE11

---
+++ C/CS210/ACE11List.c
@@ -0,0 +1,129 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "ACE9List.h"
+
+Node* new_node(char data[]){
+  Node* nodeloc = malloc(sizeof(Node));
+  nodeloc->value = data;
+  nodeloc->next = NULL;
+  return nodeloc;
+}
+
+void delete_node(Node* nodePtr){
+  free(nodePtr);
+  nodePtr = NULL;
+  }
+
+void push(List listPtr, char data[]) {
+    while (*listPtr != NULL && (*listPtr)->next != NULL) {
+        listPtr = &((*listPtr)->next);
+    }
+
+    if (*listPtr == NULL) {
+        *listPtr = new_node(data);
+    }
+    else {
+        (*listPtr)->next = new_node(data);
+    }
+}
+
+
+List new_list(){
+  List list = malloc(sizeof(Node));
+  *list = NULL;
+  return list;
+}
+
+char* pop(List listPtr){
+  while (*listPtr != NULL && (*listPtr)->next != NULL && (*listPtr)->next->next != NULL) {
+      listPtr = &((*listPtr)->next);
+  }
+
+  if (*listPtr == NULL) {
+    return NULL;
+  }
+  else if ((*listPtr)->next == NULL) {
+    char* item = (*listPtr)->value;
+    delete_node(*listPtr);
+    return item;
+  }
+  else {
+    char* item = (*listPtr)->next->value;
+    delete_node((*listPtr)->next);
+    return item;
+  }
+}
+
+void print_list(List listPtr){
+  if (*listPtr == NULL) {
+    printf("[]\n");
+  }
+  else if ((*listPtr)->next == NULL){
+    printf("[\"%s\"]\n",(*listPtr)->value);
+  }
+  else{
+    printf("[\"%s\"",(*listPtr)->value);
+    while (*listPtr != NULL && (*listPtr)->next != NULL && (*listPtr)->next->next != NULL) {
+        listPtr = &((*listPtr)->next);
+        printf(", \"%s\"",(*listPtr)->value);
+    }
+    printf(", \"%s\"]\n",(*listPtr)->next->value);
+  }
+}
+
+char* peek(List listPtr){
+  while (*listPtr != NULL && (*listPtr)->next != NULL) {
+      listPtr = &((*listPtr)->next);
+  }
+  if (*listPtr == NULL) {
+    return NULL;
+  }
+  return (*listPtr)->value;
+}
+
+void add(List listPtr, char data[]){
+  if (*listPtr == NULL) {
+    *listPtr = new_node(data);
+  }
+  else{
+    void* temp = *listPtr;
+    *listPtr = new_node(data);
+    (*listPtr)->next = temp;
+  }
+}
+
+char* rem(List listPtr){
+  if (*listPtr == NULL) {
+    return NULL;
+  }
+  else{
+    void* temp = (*listPtr)->next;
+    char* item = (*listPtr)->value;
+    delete_node(*listPtr);
+    *listPtr = temp;
+    return item;
+  }
+}
+
+int is_empty(List listPtr){
+  return (*listPtr == NULL);
+}
+
+int size(List listPtr){
+  int counter = 1;
+  if (is_empty(listPtr)){
+    return 0;
+  }
+  while (*listPtr != NULL && (*listPtr)->next != NULL) {
+      listPtr = &((*listPtr)->next);
+      counter++;
+  }
+  return counter;
+}
+
+void clear(List listPtr){
+  while (*listPtr != NULL) {
+    rem(listPtr);
+  }
+}
---
+++ C/CS210/ACE11List.h
@@ -0,0 +1,18 @@
+struct nodeType {
+  char* value;
+  struct nodeType* next;
+};
+
+typedef struct nodeType Node;
+typedef Node** List;
+
+void push(List listPtr, char data[]);
+List new_list();
+char* pop(List listPtr);
+void print_list(List listPtr);
+char* peek(List listPtr);
+void add(List listPtr, char data[]);
+char* rem(List listPtr);
+int is_empty(List listPtr);
+int size(List listPtr);
+void clear(List listPtr);
