10e17e98fbaa62006beddd0d108578de336a76f7
Fraser Patrick <fraserp2004@gmail.com>
2023-11-21 14:32:10 +0000

    added ACE8

---
+++ C/CS210/ACE8List.h
@@ -0,0 +1,12 @@
+struct nodeType {
+  char* value;
+  struct nodeType* next;
+};
+
+typedef struct nodeType Node;
+typedef Node** List;
+
+void push(List listPtr, char data[]);
+List new_list();
+char* pop(List listPtr);
+void print_list(List listPtr);
