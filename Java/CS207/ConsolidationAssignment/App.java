9c6078c54855f0ac15b193538a4a911bc387ce4c
Fraser Patrick <fraserp2004@gmail.com>
2024-01-11 19:39:10 +0000

    read from file and switch finished

--- Java/CS207/ConsolidationAssignment/App.java
+++ Java/CS207/ConsolidationAssignment/App.java
@@ -1,19 +1,70 @@
+import java.io.*;
+import java.util.*;
+
 public class App{
 	public static void main(String[] args){
-		DiGraphADT graph = new DiMaGraph(5);
-		System.out.println(graph.nNodes());
-		System.out.println(graph.nEdges());
-		System.out.println(graph.addEdge(1,2));
-		System.out.println(graph.nEdges());
-		System.out.println(graph.isEdge(1,2));
-		System.out.println(graph.isEdge(2,1));
-		System.out.println(graph.successors(1));
-		System.out.println(graph.successors(2));
-		System.out.println(graph.predecessors(1));
-		System.out.println(graph.predecessors(2));
-		System.out.println(graph.outDegree(1));
-		System.out.println(graph.outDegree(2));
-		System.out.println(graph.inDegree(1));
-		System.out.println(graph.inDegree(2));
+		int node1, node2 = 0;
+		GraphADT graph = new Graph(10);
+		
+		try (Scanner reader = new Scanner(new File("Edges.txt"))){
+			while (reader.hasNext()){
+				node1 = reader.nextInt();
+				node2 = reader.nextInt();
+				graph.addEdge(node1,node2);
+			}
+		}
+		
+		catch (FileNotFoundException e){
+			System.out.println("NO FILE FOUND");
+		}
+		
+		System.out.println("0: "+graph.neighbours(0));
+		System.out.println("1: "+graph.neighbours(1));
+		System.out.println("2: "+graph.neighbours(2));
+		System.out.println("3: "+graph.neighbours(3));
+		System.out.println("4: "+graph.neighbours(4));
+		System.out.println("5: "+graph.neighbours(5));
+		System.out.println("6: "+graph.neighbours(6));
+		System.out.println("7: "+graph.neighbours(7));
+		System.out.println("8: "+graph.neighbours(8));
+		System.out.println("9: "+graph.neighbours(9));
+		
+		Scanner myScan = new Scanner(System.in);
+		String input = "";
+		do{
+			System.out.print("Enter command: Breath, Depth or Quit:  ");
+			input = myScan.nextLine();
+
+			switch (input){
+				case "Breadth":
+					breadth();
+					break;
+
+
+				case "Depth":
+					depth();
+					break;
+
+
+				case "Quit":
+					input = "Quit";
+					System.out.println("Quiting program");
+					break;
+
+				default:
+					System.out.println("Invalid command");
+					break;
+			}
+			System.out.println("");
+		}
+		while (input != "Quit");
+	}
+	
+	public static void depth(){
+		System.out.println("DEPTH");
+	}
+	
+	public static void breadth(){
+		System.out.println("BREADTH");
 	}
 }
