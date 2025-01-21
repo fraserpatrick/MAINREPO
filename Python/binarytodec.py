a9016d86a9e2869a9c33daa539633ce2cdd73ded
Fraser Patrick <fraserp2004@gmail.com>
2025-01-21 22:01:26 +0000

    Added loop

--- Python/binarytodec.py
+++ Python/binarytodec.py
@@ -1,9 +1,10 @@
-x=input("enter binary:")
-y=""
-finalvalue=0
-for a in reversed(x):
-    y=y+a
-for i in range(len(y)):
-    if int(y[i]) == 1:
-        finalvalue = finalvalue +pow(2,i)
-print(x, "=", finalvalue)
+while(True):
+    x=input("enter binary:")
+    y=""
+    finalvalue=0
+    for a in reversed(x):
+        y=y+a
+    for i in range(len(y)):
+        if int(y[i]) == 1:
+            finalvalue = finalvalue +pow(2,i)
+    print(x, "=", finalvalue)
