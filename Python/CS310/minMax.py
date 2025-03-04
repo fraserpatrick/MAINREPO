3ec7ba15f44799f19835ca7ffd076bec08396393
Fraser Patrick <fraserp2004@gmail.com>
2025-03-04 12:32:41 +0000

    Added minimax prune

--- Python/CS310/minMax.py
+++ Python/CS310/minMax.py
@@ -34,19 +34,52 @@ def next_moves(piles):
 
 def test_timing(state):
     start = time.time()
-    value = minimax_value(state)
+    value = minimax_prune(state)
     end = time.time()
     duration = end-start
     return duration, value
 
+
+def minimax_prune(state):
+    return minimax(state, -float('inf'), float('inf'))
+
+def minimax(state, alpha, beta):
+        piles, turn = state
+        if all(p == 0 for p in piles):
+            return 1 if turn == 1 else -1
+
+        return maxPrune(state, alpha, beta) if turn == 1 else minPrune(state, alpha, beta)
+
+def maxPrune(state, alpha, beta):
+    piles, turn = state
+    best_value = -float('inf')
+    newMoves = next_moves(piles)
+
+    for new_piles in newMoves:
+        value = minimax((new_piles, 2), alpha, beta)
+        best_value = max(best_value, value)
+        alpha = max(alpha, best_value)
+        if beta <= alpha:
+            break
+    return best_value
+
+def minPrune(state, alpha, beta):
+    piles, turn = state
+    best_value = float('inf')
+    newMoves = next_moves(piles)
+
+    for new_piles in newMoves:
+        value = minimax((new_piles, 1), alpha, beta)
+        best_value = min(best_value, value)
+        beta = min(beta, best_value)
+        if beta <= alpha:
+            break
+    return best_value
+
+
 def test():
-    print("MINIMAX: " + str(minimax_value(([1,2],2))))
-    output = (test_timing(([5,5,5],1)))
+    output = (test_timing(([6,4,2,3],1)))
     print("Time taken", output[0])
     print("Value returned", output[1])
 
-#test()
-
-output = (test_timing(([5,5,5],1)))
-print("Time taken", output[0])
-print("Value returned", output[1])
+test()
