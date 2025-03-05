d8e3c216a8566eee22065ccbf9ffc3bfb221556d
Fraser Patrick <fraserp2004@gmail.com>
2025-03-05 17:25:16 +0000

    Attempt 2

--- Python/CS310/minMax.py
+++ Python/CS310/minMax.py
@@ -40,46 +40,141 @@ def test_timing(state):
     return duration, value
 
 
+visited = {}
 def minimax_prune(state):
-    return minimax(state, -float('inf'), float('inf'))
-
-def minimax(state, alpha, beta):
-        piles, turn = state
-        if all(p == 0 for p in piles):
-            return 1 if turn == 1 else -1
-
-        return maxPrune(state, alpha, beta) if turn == 1 else minPrune(state, alpha, beta)
+    visited.clear()
+    piles, turn = state
+    value = maxPrune(state, -float('inf'), float('inf')) if turn == 1 else minPrune(state, -float('inf'), float('inf'))
+    return value
 
 def maxPrune(state, alpha, beta):
     piles, turn = state
-    best_value = -float('inf')
-    newMoves = next_moves(piles)
+    if all (p==0 for p in piles):
+        return 1
 
-    for new_piles in newMoves:
-        value = minimax((new_piles, 2), alpha, beta)
-        best_value = max(best_value, value)
-        alpha = max(alpha, best_value)
-        if beta <= alpha:
-            break
-    return best_value
+    bestValue = -float('inf')
+    nextMoves = next_moves(piles)
+    for move in nextMoves:
+        value = minPrune((move,2),alpha,beta)
+        bestValue = max(bestValue, value)
+        if bestValue >= beta:
+            return bestValue
+        alpha = max(bestValue,alpha)
+    return bestValue
+
+
 
 def minPrune(state, alpha, beta):
     piles, turn = state
-    best_value = float('inf')
-    newMoves = next_moves(piles)
+    if all (p==0 for p in piles):
+        return -1
 
-    for new_piles in newMoves:
-        value = minimax((new_piles, 1), alpha, beta)
-        best_value = min(best_value, value)
-        beta = min(beta, best_value)
-        if beta <= alpha:
-            break
-    return best_value
+    bestValue = float('inf')
+    nextMoves = next_moves(piles)
+    for move in nextMoves:
+        value = maxPrune((move,1),alpha,beta)
+        bestValue = min(bestValue, value)
+        if bestValue >= alpha:
+            return bestValue
+        beta = min(bestValue,alpha)
+    return bestValue
+
+#print("Testing minimax_value: 8,8 2")
+#output = (test_timing(([8,8],2)))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
 
 
 def test():
+    print("Testing minimax_value: 8,8 2")
+    output = (test_timing(([8,8],2)))
+    print("Time taken", output[0])
+    print("Value returned", output[1])
+    print("--------------------")
+    print("Testing minimax_value: 6,4,2,3 1")
     output = (test_timing(([6,4,2,3],1)))
     print("Time taken", output[0])
     print("Value returned", output[1])
+    print("--------------------")
+    print("Testing minimax_value: 20,20 1")
+    output = (test_timing(([20,20],1)))
+    print("Time taken", output[0])
+    print("Value returned", output[1])
+    print("--------------------")
+    print("Testing minimax_value: 10,10,10 2")
+    output = (test_timing(([10,10,10],2)))
+    print("Time taken", output[0])
+    print("Value returned", output[1])
+    print("--------------------")
+    print("Testing minimax_value: 5,5,5 1")
+    output = (test_timing(([5,5,5],1)))
+    print("Time taken", output[0])
+    print("Value returned", output[1])
+    print("--------------------")
+    print("Testing minimax_value: 2,2 2")
+    output = (test_timing(([2,2],2)))
+    print("Time taken", output[0])
+    print("Value returned", output[1])
+    print("--------------------")
 
-test()
+
+#test()
+#print("Testing minimax_value: 5,5 1")
+#output = test_timing(([5, 5], 1))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
+#
+#print("\nTesting minimax_value: 5,5 1")
+#output = test_timing(([5, 5, 5], 2))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
+#
+#print("\nTesting minimax_value: 9,20,20,20,20 1")
+#output = test_timing(([9, 20, 20, 20, 20], 1))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
+#
+#print("\nTesting minimax_value: 8,8 2")
+#output = test_timing(([8, 8], 2))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
+##
+#print("\nTesting minimax_value: 4 1")
+#output = test_timing(([4], 1))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
+#
+#print("\nTesting minimax_value: 6,4,2,3 1")
+#output = test_timing(([6, 4, 2, 3], 1))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
+## #
+#print("\nTesting minimax_value: 20,20 1")
+#output = test_timing(([20, 20], 1))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
+##
+#print("\nTesting minimax_value: 10,10,10 2")
+#output = test_timing(([10, 10, 10], 2))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
+##
+#print("\nTesting minimax_value: 8,3,2 1")
+#output = test_timing(([8, 3, 2], 1))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
+#
+#print("\nTesting minimax_value: 8,8,8 1")
+#output = test_timing(([8, 8, 8], 1))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
+#
+#print("\nTesting minimax_value: 2,2 1")
+#output = test_timing(([2, 2], 1))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
+#
+#print("\nTesting minimax_value: 5,5,5 1")
+#output = test_timing(([5, 5, 5], 1))
+#print("Time taken", output[0])
+#print("Value returned", output[1])
